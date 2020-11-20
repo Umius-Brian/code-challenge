9dd8ad1f52c6833be5d84e9d282b1c2ef793a008
Brian Bui <59684534+Umius-Brian@users.noreply.github.com>
2020-11-19 22:36:22 -0800

    Render against attributes upon letter input

--- shopthrilling-api/src/Search.js
+++ shopthrilling-api/src/Search.js
@@ -1,10 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Articles from './Articles';
 import axios from 'axios';
 
 const Search = () => {
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+
+  // match search term against title, byline, and section
+  const handleChange = e => setSearchTerm(e.target.value);
+
+  // stops initial render
+  const initialRender = useRef(true);
 
 
   // fetch data from api upon initial render
@@ -14,15 +20,28 @@ const Search = () => {
       const result = await axios(
         `https://api.nytimes.com/svc/topstories/v2/science.json?api-key=${process.env.REACT_APP_API_KEY}`
       )
-      .catch(e => console.log(e))
+      .catch(e => console.log(e));
 
-      const results = result.data.results
+      const results = result.data.results;
+
+      const filteredResults = results.filter(item => {
+        if ((item.section || item.title || item.byline).includes(searchTerm)) {
+          console.log(item)
+        }
+      })
+
+      // console.log(filteredResults)
       
-      setData([]);
-      setData(results)
+      // only render upon keypress
+      if (initialRender.current) {
+        initialRender.current = false
+      } else {
+        setData([]);
+        setData(results)
+      }
     };
     fetchData();
-  })
+  }, [searchTerm])
 
   return (
     <div className='search-results'>
@@ -31,6 +50,7 @@ const Search = () => {
         id='input'
         placeholder='Search Science Articles'
         value={searchTerm}
+        onChange={handleChange}
       />
       <Articles articles={data}/>
     </div>
