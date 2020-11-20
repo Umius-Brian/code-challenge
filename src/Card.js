60274108eccd605633955ec2e18f3279acbb920a
Brian Bui <59684534+Umius-Brian@users.noreply.github.com>
2020-11-19 23:11:54 -0800

    Add links to titles

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Card = ({ title, author, section }) => {
+const Card = ({ title, author, section, url }) => {
 
   return (
     <div className='card'>
-      <h1>{title}</h1>
+      <a href={url} target='_blank'>{title}</a>
       <h2>{author}</h2> 
       <h3>{section}</h3>
     </div>
